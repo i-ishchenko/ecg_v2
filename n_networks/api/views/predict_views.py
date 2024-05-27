aa8e236a7ef0ad44598c050b951d2bf3053292ac
ViVse <ivanisenko730@gmail.com>
2024-05-27 17:54:17 +0300

    segmenting & predicting whole ecg

--- n_networks/api/views/predict_views.py
+++ n_networks/api/views/predict_views.py
@@ -1,4 +1,6 @@
 import numpy as np
+import neurokit2 as nk
+from scipy.signal import butter, filtfilt, resample, find_peaks
 import tensorflow as tf
 from tensorflow import keras
 from rest_framework import views, status
@@ -8,40 +10,47 @@ from tensorflow.keras.layers import Input
 
 class PredictView(views.APIView):
     def post(self, request, *args, **kwargs):
-        data = request.data.get('data')
-        if not data:
-            return Response({'error': 'Data not provided'}, status=status.HTTP_400_BAD_REQUEST)
+        ecg = np.array(request.data.get("ecg"), dtype=float)
+        sampling_rate = request.data.get("sampling_rate")
+        
+        data125 = self.downsample_ecg(ecg, sampling_rate, 125)
+        segments = nk.ecg_segment(data125, sampling_rate=125)
+        results = []
 
-        try:
-            # Prepare the input data
-            X = self.process_input(data, 188)
-            X = np.array(X, dtype=np.float32).reshape(1, -1)
-            X_tensor = tf.convert_to_tensor(X, dtype=tf.float32)
+        # Setup the model using TFSMLayer
+        model_folder_path = './ECG_anomaly_saved_model'
+        tfsmlayer = keras.layers.TFSMLayer(model_folder_path, call_endpoint='serving_default')
 
-            # Setup the model using TFSMLayer
-            model_folder_path = './ECG_anomaly_saved_model'
-            tfsmlayer = keras.layers.TFSMLayer(model_folder_path, call_endpoint='serving_default')
+        # Build a model around the TFSMLayer
+        inputs = Input(shape=(188,))
+        outputs = tfsmlayer(inputs)
+        model = keras.Model(inputs, outputs)
 
-            # Build a model around the TFSMLayer
-            inputs = Input(shape=(X_tensor.shape[1],))
-            outputs = tfsmlayer(inputs)
-            model = keras.Model(inputs, outputs)
+        for i in range(len(segments)):
+            try:
+                segment = segments[str(i + 1)]['Signal']
+                # Prepare the input data
+                X = self.process_input(segment, 188)
+                X = np.array(X, dtype=np.float32).reshape(1, -1)
+                X_tensor = tf.convert_to_tensor(X, dtype=tf.float32)
 
-            # Predict with the model
-            pred_dict = model.predict(X_tensor, verbose=False)
-            pred = pred_dict['output_1']  # Extract the correct output tensor from the dictionary
+                # Predict with the model
+                pred_dict = model.predict(X_tensor, verbose=False)
+                pred = pred_dict['output_1']  # Extract the correct output tensor from the dictionary
 
-            # Compute Mean Absolute Error
-            mae = MeanAbsoluteError()
-            loss = mae(X_tensor, pred).numpy()  # Ensure both inputs are tensors
-            threshold = 0.024
-            result = {"isAbnormal": loss > threshold}  # Check if loss is below threshold
+                # Compute Mean Absolute Error
+                mae = MeanAbsoluteError()
+                loss = mae(X_tensor, pred).numpy()  # Ensure both inputs are tensors
+                threshold = 0.1
+                result = {"isNormal": loss <= threshold}  # Check if loss is below threshold
+                results.append(result)
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response(result, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({"predictions": results}, status=status.HTTP_200_OK)
+
     def process_input(self, X, num):
+        X = self.normalize_data(X)
         n = len(X)
         desired_length = num
         if n < desired_length:
@@ -50,3 +59,25 @@ class PredictView(views.APIView):
         elif n > desired_length:
             X = X[:desired_length]
         return np.array(X).reshape(1, desired_length) 
+
+    def butter_filter(self, data, lowcut, highcut, fs, order=5):
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band')
+        y = filtfilt(b, a, data)
+        return y
+
+    def downsample_ecg(self, data, old_freq, new_freq):
+        # Apply a bandpass filter to retain frequencies between 0.5Hz and half of the new sampling rate
+        clean_data = self.butter_filter(data, 0.5, new_freq / 2, old_freq, order=2)
+
+        # Calculate the number of samples in the downsampled data
+        num_samples = int(len(data) * (new_freq / old_freq))
+
+        # Resample the data
+        downsampled_data = resample(clean_data, num_samples)
+        return downsampled_data
+
+    def normalize_data(self, data):
+        return (data - np.min(data)) / (np.max(data) - np.min(data))
