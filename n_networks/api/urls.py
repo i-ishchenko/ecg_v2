7a9bb403bcaec486006021ce6dce21ef4cc52a98
ViVse <ivanisenko730@gmail.com>
2024-05-31 13:40:51 +0300

    remove separate classify endpoint

--- n_networks/api/urls.py
+++ n_networks/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import ClassifyView, PredictView, ProcessView
 
 urlpatterns = [
-  path('classify/', ClassifyView.as_view(), name='classify'),
   path('predict/', PredictView.as_view(), name='predict'),
   path('process/', ProcessView.as_view(), name='process')
 ]
