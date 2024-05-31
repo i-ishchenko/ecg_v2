b8b45f51ee4c35f5ae1b132a5aa25caa1c464c68
ViVse <ivanisenko730@gmail.com>
2024-05-31 13:43:16 +0300

    fix exports

--- n_networks/api/urls.py
+++ n_networks/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ClassifyView, PredictView, ProcessView
+from .views import PredictView, ProcessView
 
 urlpatterns = [
   path('predict/', PredictView.as_view(), name='predict'),
--- n_networks/api/views/__init__.py
+++ n_networks/api/views/__init__.py
@@ -1,3 +1,2 @@
-from .classify_views import *
 from .predict_views import *
 from .process_views import *
