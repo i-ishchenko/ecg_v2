81b712c4559e0ff9bec0cd989dd2e1663dec645f
ViVse <ivanisenko730@gmail.com>
2024-06-08 15:38:19 +0300

    loading progress bar

--- frontend/lib/providers.tsx
+++ frontend/lib/providers.tsx
@@ -3,6 +3,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,12 @@ export default function Providers({
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
+        <ProgressBar
+          height="2px"
+          color="black"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
         {children}
         <Toaster />
       </QueryClientProvider>
